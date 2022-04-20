5697d480263d7a7dc4810574d7dc12431cec8e2c
Tsathoggua of Nkai <5774045+tsathoqqua@users.noreply.github.com>
2022-04-20 18:38:16 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LIMA Mudlib'
+copyright = '2022, LIMA Mudlib team'
+author = 'LIMA Mudlib team'
 
 release = '0.1'
 version = '0.1.0'
