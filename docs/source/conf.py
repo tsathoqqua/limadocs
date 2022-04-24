140d2a0ff34f71f7fb6ae3defc3cd00aa616a432
Rajo <69515836+Zechsu@users.noreply.github.com>
2022-04-24 22:36:10 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,14 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
