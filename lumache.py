dbeacb724b62ee9e5f98921feba71775e888c22e
sorenstaun <113976403+sorenstaun@users.noreply.github.com>
2024-03-21 11:11:23 +0100

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -1,23 +1,9 @@
 """
-Lumache - Python library for cooks and food lovers.
+Lumache - Python library LIMA Mudlib.
 """
 
-__version__ = "0.1.0"
-
+__version__ = "0.2.0"
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
     pass
-
-
-def get_random_ingredients(kind=None):
-    """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
